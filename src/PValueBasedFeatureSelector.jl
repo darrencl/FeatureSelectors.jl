b66ede288d33201f04028b2c85471a31a55855e8
Darren Lukas <darren.lukas@tri.edu.au>
2020-03-04 13:02:33 +1000

    Fixed p-val selector and Chisq test

--- src/PValueBasedFeatureSelector.jl
+++ src/PValueBasedFeatureSelector.jl
@@ -36,30 +36,49 @@ function select_features(selector::PValueBasedFeatureSelector,
             [pvalue(VarianceFTest(X_col, y))
              for X_col in eachcol(X_data)]
         elseif selector.method == :ChiSq
-            # Temporary DataFrame for easy aggregation
-            tmp_df = DataFrame(hcat(X_data, y))
+            # DataFrame for easy aggregation
+            data_df = DataFrame(hcat(X_data, y))
             # Rename cols to prevent name clash with count aggregation below
-            names!(tmp_df, [Symbol("input$i") for i=1:size(tmp_df)[2]])
-            y_name = names(tmp_df[!, [end]])[1]
+            rename!(data_df, [Symbol("input$i") for i=1:size(data_df)[2]])
+            y_name = names(data_df[!, [end]])[1]
             tmp_pvals = Vector{Float64}()
-            for (col_name, X_col) in eachcol(tmp_df[:, 1:end-1], true)
-                _y = by(tmp_df, [col_name, y_name], nrow)
-                @info _y
-                push!(tmp_pvals, pvalue(ChisqTest(reshape(_y.x1, (2,2)))))
+            for (col_name, X_col) in eachcol(data_df[:, 1:end-1], true)
+                # Hacky method to generate frequency count and put 0 for missing
+                # combination. `by` function is not able to achieve this. See:
+                # https://github.com/JuliaData/DataFrames.jl/issues/2136
+
+                # Feature + target to process and put count = 1 for each row
+                Xy_df = hcat(data_df[:, [col_name, y_name]], Int.(ones(size(data_df)[1])))
+                rename!(Xy_df, :x1=>:Count)
+                # This nested join will append missing in Count to missing combination
+                # of value in col_name and y_name
+                Xy_df = join(
+                    join(
+                        DataFrame(Dict(col_name=>unique(Xy_df[:, col_name]))),
+                        DataFrame(Dict(y_name=>unique(Xy_df[:, y_name]))),
+                        kind=:cross
+                    ),
+                    Xy_df,
+                    on=[col_name,y_name],
+                    kind=:outer
+                )
+                # sum count and recode missing to 0
+                _y = recode(sort(by(Xy_df, [col_name, y_name], r->sum(r.Count))).x1, missing=>0)
+                _y = reshape(_y, (length(unique(Xy_df[:, col_name])), length(unique(Xy_df[:, y_name]))))
+                push!(tmp_pvals, pvalue(ChisqTest(_y)))
             end
             tmp_pvals
         end
     end
     sorted_tup = sort([x for x in zip(X_features, pvals)],
-                      by=v-> abs(v[2]),
-                      rev=true)
+                      by=v-> abs(v[2]))
     if selector.k > 0
         verbose && @info "Filtering top k features" selector.k
         sorted_tup = sorted_tup[1:selector.k]
     end
     if selector.threshold > 0.0
         verbose && @info "Filtering by threshold" selector.threshold
-        sorted_tup = filter(v-> v[2]>=selector.threshold, sorted_tup)
+        sorted_tup = filter(v-> v[2]<=selector.threshold, sorted_tup)
     end
     if return_val
         sorted_tup
